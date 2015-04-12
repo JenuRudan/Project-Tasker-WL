9e817a6b5a0e7022b457d565b5ba1c3a381042ed
JenuRudan <mastertofe@gmail.com>
2015-04-12 21:48:18 +0200

    Uploading Project
    NONE

---
+++ README.md
@@ -0,0 +1,3 @@
+# Project Tasker
+Android hybrid application via worklight
+ Copyright 2015 Mustafa Barakat
---
+++ Worklight/apps/TestData/android/native/assets/www/default/worklight/checksum.js
@@ -0,0 +1,2 @@
+var WL_CHECKSUM = {"date":1428830428515,"machine":"MustafaBarakat","checksum":1306085983};
+/* Date: Sun Apr 12 11:20:28 EET 2015 */
